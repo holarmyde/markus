48a041065072336d8c29f4397b756c53b57b09e5
Oluwafemi Aremu <ooa255@nyu.edu>
2019-09-25 09:50:26 -0400

    fc

---
+++ graceinc/config/dev.js
@@ -0,0 +1,31 @@
+
+module.exports = {
+  googleClientID:
+    '931292097446-ivsh3ecvs15177nn5gngil554hvrr1p3.apps.googleusercontent.com',
+  googleClientSecret: 'NcF9E0i7qkHhBiDu9ulFXf1g',
+  mongoURI: 'mongodb://aremu:oluwafemi1@ds155626.mlab.com:55626/blog-tutorial-real',
+  cookieKey: '123123123',
+  accessKeyId: 'AKIAYVJQCW7R4IS6WIMZ',
+  secretAccessKey: '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',
+  redisUrl: 'redis://h:pc473b2ecfa4947c37b9785ac93920f88964cfc128a3b79507cfa95f2d31573e0@ec2-34-206-10-16.compute-1.amazonaws.com:14059'
+
+
+};
+
+
+/**
+ *
+ * module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://aremu:oluwafemi1@ds155626.mlab.com:55626/blog-tutorial-real',
+  cookieKey: '123123123',
+  accessKeyId: 'AKIAYVJQCW7R4IS6WIMZ',
+  secretAccessKey: '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',
+  redisUrl: 'redis://h:p9090de15f3f21c929a7cd3f069c6e9cfca226d84ea599bf97e95ad3b620f5822@ec2-52-1-169-125.compute-1.amazonaws.com:12679'
+
+
+};
+ *
+ */
